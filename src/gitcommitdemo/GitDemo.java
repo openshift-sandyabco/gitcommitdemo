e4affdad94ffd4a26624657a16284cbb96f1e96e
openshift-sandyabco <sandylaabco@gmail.com>
2023-07-20 22:22:44 -0400

    In class commit

--- src/gitcommitdemo/GitDemo.java
+++ src/gitcommitdemo/GitDemo.java
@@ -3,8 +3,7 @@ package gitcommitdemo;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		System.out.println("First Java Program for Git - First commit");
+		System.out.println("First Java Program for Git. - In class demo");
  
 	}
-
 }
